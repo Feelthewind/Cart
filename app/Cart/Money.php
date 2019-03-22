0fe25b4557ee46bb846c592acc7c42d245e2f7b9
lcs <skyisthelimitlessW@gmail.com>
2019-03-22 19:11:13 +0900

    product variation prices

--- app/Cart/Money.php
+++ app/Cart/Money.php
@@ -44,4 +44,3 @@ class Money
     return $this->money;
   }
 }
-
--- app/Models/ProductVariation.php
+++ app/Models/ProductVariation.php
@@ -3,9 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\HasPrice;
+use App\Cart\Money;
 
 class ProductVariation extends Model
 {
+    use HasPrice;
+
+    public function getPriceAttribute($value)
+    {
+        if ($value === null) {
+            return $this->product->price;
+        }
+
+        return new Money($value);
+    }
+
+    public function priceVaries()
+    {
+        return $this->price->amount() !== $this->product->price->amount();
+    }
+
     public function type()
     {
         return $this->hasOne(ProductVariationType::class, 'id', 'product_variation_type_id');
