2a6a51d95da280c5209e403d272531b235de2f4e
lcs <skyisthelimitlessW@gmail.com>
2019-03-24 01:57:35 +0900

    the me endpoint

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,7 +14,8 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
+        if (!$request->expectsJson()) {
+            // dd('Authenticate Middleware!!');
             return route('login');
         }
     }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+
+// Auth::routes();
+
 Route::resource('categories', 'Categories\CategoryController');
 Route::resource('products', 'Products\ProductController');
 
 Route::group(['prefix' => 'auth'], function () {
   Route::post('register', 'Auth\RegisterController@action');
   Route::post('login', 'Auth\LoginController@action');
+  Route::get('me', 'Auth\MeController@action');
 });
