f2d948bc85db44a4d196ef5bccd10eacba9e5c1f
lcs <skyisthelimitlessW@gmail.com>
2019-03-26 01:33:34 +0900

    testing minimum stock

--- tests/Unit/Models/Products/ProductVariationTest.php
+++ tests/Unit/Models/Products/ProductVariationTest.php
@@ -147,4 +147,17 @@ class ProductVariationTest extends TestCase
 
         $this->assertEquals($variation->stockCount(), 10);
     }
+
+    public function test_it_can_get_the_minimum_stock_for_a_given_value()
+    {
+        $variation = factory(ProductVariation::class)->create();
+
+        $variation->stocks()->save(
+            factory(Stock::class)->make([
+                'quantity' => $quantity = 5
+            ])
+        );
+
+        $this->assertEquals($quantity, $variation->minStock(200));
+    }
 }
