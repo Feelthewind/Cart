3d275914ed4be62bfc76ed2ea1d5ddb3ade3821b
lcs <skyisthelimitlessW@gmail.com>
2019-03-31 01:35:05 +0900

    setting up stripe

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use App\Cart\Cart;
+use Stripe\Stripe;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -33,6 +34,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Schema::defaultStringLength(191);
+        Stripe::setApiKey(config('services.stripe.secret'));
     }
 }
