e27f1048f91f80b7db5d3e854e6209eb68eebe46
lcs <skyisthelimitlessW@gmail.com>
2019-03-30 15:35:54 +0900

    fixing a syncing bug

--- tests/Unit/Cart/CartTest.php
+++ tests/Unit/Cart/CartTest.php
@@ -163,16 +163,22 @@ class CartTest extends TestCase
             $user = factory(User::class)->create()
         );
 
-        $user->cart()->attach(
-            $product = factory(ProductVariation::class)->create(),
-            [
+        $product = factory(ProductVariation::class)->create();
+        $anotherProduct = factory(ProductVariation::class)->create();
+
+        $user->cart()->attach([
+            $product->id => [
                 'quantity' => 2
-            ]
-        );
+            ],
+            $anotherProduct->id => [
+                'quantity' => 2
+            ],
+        ]);
 
         $cart->sync();
 
         $this->assertEquals(0, $user->fresh()->cart->first()->pivot->quantity);
+        $this->assertEquals(0, $user->fresh()->cart->get(1)->pivot->quantity);
     }
 
     public function test_it_can_check_if_the_cart_has_changed_after_syncing()
@@ -180,13 +186,18 @@ class CartTest extends TestCase
         $cart = new Cart(
             $user = factory(User::class)->create()
         );
+        
+        $product = factory(ProductVariation::class)->create();
+        $anotherProduct = factory(ProductVariation::class)->create();
 
-        $user->cart()->attach(
-            $product = factory(ProductVariation::class)->create(),
-            [
+        $user->cart()->attach([
+            $product->id => [
                 'quantity' => 2
-            ]
-        );
+            ],
+            $anotherProduct->id => [
+                'quantity' => 0
+            ],
+        ]);
 
         $cart->sync();
 
