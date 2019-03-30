9e343f1cefe74404d74fb594d9798f466bf10555
lcs <skyisthelimitlessW@gmail.com>
2019-03-30 15:10:21 +0900

    product variation product relationship

--- app/Http/Resources/ProductVariationResource.php
+++ app/Http/Resources/ProductVariationResource.php
@@ -26,7 +26,8 @@ class ProductVariationResource extends JsonResource
             'price_varies' => $this->priceVaries(),
             'stock_count' => (int)$this->stockCount(),
             'type' => $this->type->name,
-            'in_stock' => $this->inStock()
+            'in_stock' => $this->inStock(),
+            'product' => new ProductIndexResource($this->product)
         ];
     }
 }
