3c224154ed20755e7f25f0b3197c941da57eec4f
lcs <skyisthelimitlessW@gmail.com>
2019-03-31 02:39:00 +0900

    storing a payment method

--- app/Cart/Payments/Gateways/StripeGateway.php
+++ app/Cart/Payments/Gateways/StripeGateway.php
@@ -4,7 +4,7 @@ namespace App\Cart\Payments\Gateways;
 
 use App\Cart\Payments\Gateway;
 use App\Models\User;
-
+use Stripe\Customer as StripeCustomer;
 
 class StripeGateway implements Gateway
 {
@@ -17,8 +17,41 @@ class StripeGateway implements Gateway
     return $this;
   }
 
+  public function user()
+  {
+    return $this->user;
+  }
+
   public function createCustomer()
   {
-    return new StripeGatewayCustomer();
+    if ($this->user->gateway_customer_id) {
+      return $this->getCustomer();
+    }
+
+    $customer = new StripeGatewayCustomer(
+      $this,
+      $this->createStripeCustomer()
+    );
+
+    $this->user->update([
+      'gateway_customer_id' => $customer->id()
+    ]);
+
+    return $customer;
+  }
+
+  protected function getCustomer()
+  {
+    return new StripeGatewayCustomer(
+      $this,
+      StripeCustomer::retrieve($this->user->gateway_customer_id)
+    );
+  }
+
+  protected function createStripeCustomer()
+  {
+    return StripeCustomer::create([
+      'email' => $this->user->email
+    ]);
   }
 }
--- app/Cart/Payments/Gateways/StripeGatewayCustomer.php
+++ app/Cart/Payments/Gateways/StripeGatewayCustomer.php
@@ -4,11 +4,41 @@ namespace App\Cart\Payments\Gateways;
 
 use App\Cart\Payments\GatewayCustomer;
 use App\Models\PaymentMethod;
+use App\Cart\Payments\Gateway;
+use Stripe\Customer as StripeCustomer;
 
 class StripeGatewayCustomer implements GatewayCustomer
 {
+  protected $gateway;
+  protected $customer;
+
+  public function __construct(Gateway $gateway, StripeCustomer $customer)
+  {
+    $this->gateway = $gateway;
+    $this->customer = $customer;
+  }
+
   public function charge(PaymentMethod $card, $amount)
   { }
+
   public function addCard($token)
-  { }
+  {
+    $card = $this->customer->sources->create([
+      'source' => $token
+    ]);
+
+    $this->customer->default_source = $card->id;
+    $this->customer->save();
+
+    $this->gateway->user()->paymentMethods()->create([
+      'provider_id' => $card->id,
+      'card_type' => $card->brand,
+      'last_four' => $card->last4
+    ]);
+  }
+
+  public function id()
+  {
+    return $this->customer->id;
+  }
 }
