dc248969fbf95ad4c12c3b936a9156d6af382cf0
lcs <skyisthelimitlessW@gmail.com>
2019-03-31 18:39:37 +0900

    storing transactions

---
+++ tests/Unit/Listeners/CreateTransactionListenerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit\Listeners;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Order;
+use App\Events\Order\OrderPaid;
+use App\Listeners\Order\CreateTransaction;
+
+class CreateTransactionListenerTest extends TestCase
+{
+  /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+  public function test_it_creates_a_transaction()
+  {
+    $event = new OrderPaid(
+      $order = factory(Order::class)->create([
+        'user_id' => factory(User::class)->create()
+      ])
+    );
+
+    $listener = new CreateTransaction();
+
+    $listener->handle($event);
+
+    $this->assertDatabaseHas('transactions', [
+      'order_id' => $order->id,
+      'total' => $order->total()->amount()
+    ]);
+  }
+}
